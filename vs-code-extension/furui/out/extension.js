f9b23810ed7298e838360b9d423f9f9a7d07db7e
untrulynoxiusmj <chetangarg365@gmail.com>
2020-12-19 19:56:14 +0530

    beautiful webview

--- vs-code-extension/furui/out/extension.js
+++ vs-code-extension/furui/out/extension.js
@@ -15,6 +15,7 @@ const axios_1 = require("axios");
 const express = require("express");
 function activate(context) {
     console.log('Congratulations, your extension "furui" is now active!');
+    context.globalState.update("page", 1);
     let currentPanel = undefined;
     context.subscriptions.push(vscode.commands.registerCommand("furui.login", () => {
         const app = express();
@@ -116,6 +117,7 @@ function activate(context) {
         }));
     })));
     let show = (currentPanel) => __awaiter(this, void 0, void 0, function* () {
+        console.log("show");
         let result;
         let page = context.globalState.get("page");
         let token = context.globalState.get("token");
@@ -132,6 +134,7 @@ function activate(context) {
                 Authorization: `token ${token}`,
             },
         });
+        console.log(resultbackend);
         vscode.window.showInformationMessage("Successfully retrieved codes from database");
         console.log(resultbackend.data);
         currentPanel.webview.html = getWebviewContent(resultbackend);
@@ -153,23 +156,172 @@ function activate(context) {
             currentPanel = undefined;
         }, null, context.subscriptions);
     })));
-    context.subscriptions.push(vscode.commands.registerCommand('furui.next', () => __awaiter(this, void 0, void 0, function* () {
+    context.subscriptions.push(vscode.commands.registerCommand("furui.next", () => __awaiter(this, void 0, void 0, function* () {
         let page = context.globalState.get("page");
         context.globalState.update("page", page + 1);
-        vscode.commands.executeCommand('furui.getCode');
+        vscode.commands.executeCommand("furui.getCode");
     })));
-    context.subscriptions.push(vscode.commands.registerCommand('furui.prev', () => __awaiter(this, void 0, void 0, function* () {
+    context.subscriptions.push(vscode.commands.registerCommand("furui.prev", () => __awaiter(this, void 0, void 0, function* () {
         let page = context.globalState.get("page");
         if (page > 0) {
             context.globalState.update("page", page - 1);
         }
-        vscode.commands.executeCommand('furui.getCode');
+        vscode.commands.executeCommand("furui.getCode");
     })));
 }
 exports.activate = activate;
 function deactivate() { }
 exports.deactivate = deactivate;
 function getWebviewContent(resultbackend) {
-    return `<html>${resultbackend.data.code}</html>`;
+    let element = resultbackend.data;
+    let allCodes = `
+    
+    <!DOCTYPE html>
+
+	<html>
+	
+	
+		<head>
+
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.1/styles/nord.min.css" integrity="sha512-igI4zzTHEU3IASS/ojMD7tO6hScqpnEnz41u+xVRNZvZEaF3XaCdre0qZ08frR1hri9+aSNeAXlQz1DS3luvxA==" crossorigin="anonymous" />
+        
+            <style>
+                pre > code {
+                    font-family: 'Fira Code', monospace;
+                    
+                    margin-left: auto;
+                    margin-right: auto;
+                    height: 60vh;
+                    width: 80vh;
+                    overflow-x:auto;
+                    overflow-y:auto;
+                }
+
+                body {
+                    vscode-light {
+                        color: black;
+                    }
+                      
+                    vscode-dark {
+                        color: white;
+                    }
+                      
+                    vscode-high-contrast {
+                        color: red;
+                    }
+
+                    font-size: medium;
+                    padding : 2em;
+                    font-family: 'Fira Code', monospace;
+                }
+
+                .all{
+                    display:flex;
+                    flex-wrap:wrap;
+                    justify-content: space-evenly;
+                }
+
+                .inc, .comm{
+                    font-size:5vh;
+                }
+                .commentbox {
+                    
+                    margin-left: auto;
+                    margin-right: auto;
+                    height: 60vh;
+                    overflow-y:auto;
+                }
+
+                textarea{
+                    color: var(--vscode-editor-foreground);
+                    width: 60vh;
+                    background-color: var(--vscode-editor-background);
+                    box-sizing: border-box;
+                    font-size:large
+                }
+
+                img{
+                    
+                    display: inline-block;
+                    border-radius: 50%;
+                    width : 4vh;
+                }
+
+                .pro {
+                        display: flex;
+                        align-items:center;
+                        font-size:4vh;
+                }
+
+                .four {
+                    font-size:4vh;	
+                }
+                
+            </style>
+        
+		</head>
+	
+		<body>
+    
+            <div class=all>
+                <div class=one>
+                    <div class=pro>
+                        <img src = ${element.image} onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgRlaLA5Gg9NCwxcz30Go5FST9L9_OYeNkyQ&usqp=CAU';">&nbsp;
+                        <span>${element.username}</span>
+                        <span>:</span>
+                        <span>${element.title}</span>
+                    </div>
+                    <pre>
+                        <code>
+                            ${element.code}
+                        </code>
+                    </pre>
+                
+                    <div class=inc>
+                        <span>${element.likes}</span>
+                        <span class=like onclick="like('${element._id.toString()}')">++</span>
+                    </div>
+                </div>
+            
+                <div class="commentBox">
+                    <span class=four>Comments</span>
+                    <pre>
+                        <code>
+                            ${JSON.stringify(element.comments, undefined, 4)}
+                        </code>
+                    </pre>
+                    <div class="textarea-container">
+                        <textarea name="Write your Comment" id="ta" placeholder="Write your Comment"></textarea>
+                        <span class=comm onclick="comment('${element._id.toString()}')">() => </span>
+                    </div>
+                </div>
+            </div>
+        
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.1/highlight.min.js"></script>
+	        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+	        <script>
+
+                hljs.initHighlightingOnLoad();
+                const vscode = acquireVsCodeApi();
+                console.log("hello");
+                function like(id){
+                }
+                function comment(id){
+                }
+
+            </script>
+            
+        </body>
+
+	    <script>
+            if (${element.liked}){
+                document.querySelector(".like").textContent = "--";
+            }
+	    </script>
+
+    </html>`;
+    return allCodes;
 }
 //# sourceMappingURL=extension.js.map
